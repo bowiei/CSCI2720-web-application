6ddda6c88f081fdbac56c0f0f98dab5102a86814
jeff4121336 <jeffyau36@gmail.com>
2023-12-05 16:42:17 +0800

    header spearated

---
+++ src/components/admin/header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Title extends React.Component {
+    render() {
+        return (
+            <header className="bg-warning">
+            <h1 className="display-4 text-center">{this.props.name}</h1> </header>
+        ); 
+    }
+}
+
+class Header extends React.Component{
+    render(){
+        return (
+            <Title name={this.props.name} />
+        );
+    }
+}
+
+export default Header;
--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Header from './components/Header';
 
-class Title extends React.Component {
-    render() {
-        return (
-            <header className="bg-warning">
-            <h1 className="display-4 text-center">{this.props.name}</h1> </header>
-        ); 
-    }
-}
-
-class App extends React.Component{
-    render(){
-        return (
-            <Title name={this.props.name} />
-        );
-    }
-}
-
-const root = ReactDOM.createRoot(document.querySelector("#app"));
-root.render(<App name="Project"/>);
+const root = ReactDOM.createRoot(document.querySelector("#header"));
+root.render(<Header name="Project"/>);
