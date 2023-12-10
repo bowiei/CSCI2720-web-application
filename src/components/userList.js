d92aa827161370a7af0b4bf2c9bfbb1e8022844a
jeff4121336 <jeffyau36@gmail.com>
2023-12-10 21:51:38 +0800

    edit components

--- src/components/userList.js
+++ src/components/userList.js
@@ -19,7 +19,7 @@ export default class UserList extends Component {
 
   componentDidMount() {
     axios
-      .get("http://localhost:5000/user/")
+      .get("http://localhost:5500/user/")
       .then((response) => {
         this.setState({ users: response.data });
       })
--- src/components/venueList.js
+++ src/components/venueList.js
@@ -37,16 +37,6 @@ class VenueList extends Component {
     const filteredVenues = venues.filter((venue) => venue.address.toLowerCase().includes(filterKeyword.toLowerCase()));
     this.setState({ venues: filteredVenues });
   };
-  handleInputChange = (e) => {
-    this.setState({ filterKeyword: e.target.value });
-  };
-
-  handleFilter = () => {
-    // Filter the table based on the filterKeyword
-    const { venues, filterKeyword } = this.state;
-    const filteredVenues = venues.filter((venue) => venue.address.toLowerCase().includes(filterKeyword.toLowerCase()));
-    this.setState({ venues: filteredVenues });
-  };
 
   handleSort = () => {
     // Sort the table based on the sortByEvent (asc or desc)
