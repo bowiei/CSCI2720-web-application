55411e266de3e6b3a1420881fa1558b21b8c3fae
bowiei <jasoni4399@gmail.com>
2023-12-12 22:09:40 +0800

    map complete

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
+const logout={
+    margin:'3px',
+    float:'right',
+    right:"0px",
+}
+
+const header={
+    height:'5vh',
+    font_size:'5px',
+}
+
 class Title extends React.Component {
     render() {
         return (
-            <header className="bg-warning">
-            <h1 className="display-4 text-center">{this.props.name}</h1> </header>
+            <h2 className="display-7 text-center">{this.props.name}</h2>
         ); 
     }
 }
@@ -12,7 +22,10 @@ class Title extends React.Component {
 class Header extends React.Component{
     render(){
         return (
-            <Title name={this.props.name} />
+            <header className="bg-warning" style={header}>
+                <button style={logout} class="btn btn-danger btn-sm">{this.props.user} Log out</button>
+                <Title name={this.props.name}/>
+            </header>
         );
     }
 }
--- src/pages/user.js
+++ src/pages/user.js
@@ -1,76 +1,116 @@
-import React from "react";
-import Header from "../components/header.js";
-import Form from "../components/form.js";
-import LocationView from "../components/venueList.js";
-import CommentList from "../components/comment.js";
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import React,{useState,useRef,useEffect} from 'react';
+import Header from '../components/header.js';
+import Form from '../components/form.js';
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 
-const libraries = ["places"];
-const mapstyle = {
-  display: "inline-block",
-  margin: "1%",
-  border: "2px solid black",
-  width: "75vw",
-  background: "gray",
-};
+const libraries = ['places'];
+const mapstyle={
+  display:'inline-block',
+  margin:'1%',
+  border:'2px solid black',
+  width: '75vw',
+  background:'gray',
+}
+
 const mapContainerStyle = {
-  display: "inline-block",
-  margin: "1%",
-  border: "2px solid black",
-  width: "49vw",
-  height: "40vh",
+  display:'inline-block',
+  margin:'1%',
+  border:'2px solid black',
+  width: '49vw',
+  height: '40vh',
 };
 
 let center = {
-  lat: 7.2905715, // default latitude
-  lng: 80.6337262, // default longitude
+  lat: 7.2905715,
+  lng: 80.6337262,
 };
 
-let zoom = 20;
+let zoom=20;
 
-function chooseLocation() {}
+function chooseLocation(pos){
 
-let loc = {
-  venue: "123",
-  address: "111",
-  event: "111",
+}
+
+let loc={
+  venue:'123',
+  address:'111',
+  event:'111',
 };
 
 class UserPage extends React.Component {
   constructor(props) {
     super(props);
+    this.state={
+      zoom:20,
+      center:{
+        lat: 22.35665,
+        lng: 114.12623,
+      },
+      venuelist:[
+        {lat: 22.35665,lng: 114.12623,},
+        {lat: 22.36665,lng: 114.12623,},
+        {lat: 22.37665,lng: 114.12623,},
+        {lat: 22.38665,lng: 114.12623,},
+        {lat: 22.39665,lng: 114.12623,},
+        {lat: 22.40665,lng: 114.12623,},
+        {lat: 22.41665,lng: 114.12623,},
+        {lat: 22.42665,lng: 114.12623,},
+        {lat: 22.43665,lng: 114.12623,},
+        {lat: 22.44665,lng: 114.12623,},
+      ],
+    }
+  }
+
+  centerUpdate(){
+    this.setState({center:{lat:0,lng:0}})
   }
   render() {
     return (
       <div>
-        <Header name="User Page" user={this.props.user} />
-        <div style={mapstyle} class="card d-inline-block m-2">
-          <Mapp />
-          <LocationView loc={loc} />
-        </div>
-        <Form name="user Comment" />
+        <Header name="User Page" user='{this.props.user}'/>
+          <div style={mapstyle} class="card d-inline-block m-2">
+            <Mapp center={this.state.center} venuelist={this.state.venuelist}/>
+            <venueList></venueList>
+            <button onClick={()=>{this.centerUpdate()}}>press mes</button>
+          </div>
       </div>
     );
   }
 }
 
-const Mapp = () => {
+function Mapp(props){
+  const[pos,position]=useState(props.center);
+  const[venue,venuePos]=useState(props.venuelist);
+
+  useEffect(()=>position(props.center))
+  useEffect(()=>venuePos(props.venuelist))
   const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: "AIzaSyDHzobelxj6hZ2p8gwxk3jpAkTRcXJfVYU",
+    googleMapsApiKey: 'AIzaSyDHzobelxj6hZ2p8gwxk3jpAkTRcXJfVYU',
     libraries,
   });
 
+  function setCenter(i,e){
+    if(props.key=this.i)
+    props.center=e;
+    console.log(e)
+  }
+  
   if (loadError) {
     return <div>Error loading maps</div>;
   }
-
+  
   if (!isLoaded) {
     return <div>Loading maps</div>;
   }
-
+  let i=-1;
   return (
-    <GoogleMap mapContainerStyle={mapContainerStyle} zoom={zoom} center={center}>
-      <Marker position={center} onclick="chooseLocation()" />
+    <GoogleMap id='map' mapContainerStyle={mapContainerStyle} zoom={20} center={pos}>
+      {
+       venue.map((element) => {
+        i++;
+        return(<Marker position={element} key={i} onclick={(element)=>setCenter(i,element) }/>)
+      })
+      }
     </GoogleMap>
   );
 };
