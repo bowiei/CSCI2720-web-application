09033b90ec854af923c2abbe741285acd37792d1
bowiei <jasoni4399@gmail.com>
2023-12-13 21:15:49 +0800

    separate view update

--- src/components/seperateView.js
+++ src/components/seperateView.js
@@ -15,15 +15,19 @@ const venuestyle={
 }
 
 const venuetitlestyle={
-    margin: "1%"
+    padding:"1%",
+    border: "2px solid black",
+    margin: "1%",
 }
 
 const eventstyle={
+    margin:"1%",
+    padding:"1%",
     display: "inline-block",
     border: "2px solid black",
     overflow: "x",
     overflow: "y",
-    weight:"100%",
+    weight:"75vw",
 }
 
 class Eventblock extends React.Component{
@@ -31,25 +35,25 @@ class Eventblock extends React.Component{
         super(props)
         this.state={
             title:'',
-            progtimee:'',
+            progtimee:'', 
             date:'',
             price:'',
             description:'',
             presenterorge:''
         }
-        axios.get(`http://localhost:5500/event/${this.props.eventid}`)
-            .then((res)=>{
-                this.setState({title:res.data.title});
-                this.setState({progtimee:res.data.progtimee});
-                this.setState({date:res.data.date});
-                this.setState({price:res.data.price});
-                this.setState({description:res.data.description});
-                this.setState({presenterorge:res.data.presenterorge});
-                console.log('the event is',res.data);
-            })
     }
 
     render(){
+        axios.get(`http://localhost:5500/event/${this.props.eventid}`)
+                .then((res)=>{
+                    this.setState({title:res.data.title});
+                    this.setState({progtimee:res.data.progtimee});
+                    this.setState({date:res.data.date});
+                    this.setState({price:res.data.price});
+                    this.setState({description:res.data.description});
+                    this.setState({presenterorge:res.data.presenterorge});
+                    console.log('the event is',res.data);
+                })
         return(
             <div style={eventstyle} className="card d-inline-block m-2">
                 <p>Title: {this.state.title}</p>
@@ -78,13 +82,14 @@ class SeperateView extends React.Component{
             })
         )
     }
-    
+
     render(){
         return(
             <div style={viewstyle} className="card d-inline-block m-2">
-                <div style={venuetitlestyle}>{this.props.venuedetails.address}</div>
-                <div style={venuestyle}>
-                        {this.updateEventlist(this.props.venuedetails.events)}
+                <div style={venuetitlestyle} className="card d-inline-block m-2">
+                    Location: {this.props.venuedetails.address}</div>
+                <div >
+                    {this.updateEventlist(this.props.venuedetails.events)}
                 </div>
             </div>
         )
