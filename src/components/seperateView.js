e4dde9b581ff56beb8dd9e17859083abcd1469a8
jeff4121336 <jeffyau36@gmail.com>
2023-12-15 13:35:54 +0800

    remove comments

--- src/components/seperateView.js
+++ src/components/seperateView.js
@@ -1,50 +1,7 @@
 import React from "react";
-import axios from "axios";
 import Map from '../components/map';
 
-// const viewstyle = {
-//     display: "inline-block",
-//     margin: "1%",
-//     border: "2px solid black",
-//     width: "75vw",
-//     background: "gray",
-// };
-
-// const venuestyle={
-//     weight:"30vw",
-//     overflow: "y",
-// }
-
-// const venuetitlestyle={
-//     padding:"1%",
-//     border: "2px solid black",
-//     margin: "1%",
-// }
-
-// const commentstyleblock={
-//     display: "inline-block",
-//     margin: "1%",
-//     border: "2px solid black",
-//     width: "20vw",
-//     background: "gray",
-// }
-
-
-// Eventblock -> EventCard
-// Add more Locationblock -> save in LocationCard
-
 class SeperateView extends React.Component{
-
-    // updateEventlist(a){
-    //     return(
-    //         a?.map((u)=>{
-    //             console.log("u=",u);
-    //             return (
-    //                 <Eventblock eventid={u}/>
-    //             )
-    //         })
-    //     )
-    // }
 
     render(){
         return(
@@ -56,35 +13,10 @@ class SeperateView extends React.Component{
                     </div>
                 </div>
                 </div>
-                {/* <div className="row">
-                    <Map/>
-                </div>
-                <div className="row">
-                    <div className="col-md-6">
-                        <div className="card">
-                            <div className="card-body">
-                                Add Comment 4c
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-6">
-                        <div className="card">
-                            <div className="card-body">
-                                Show Comment 4c
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div style={viewstyle} className="card d-inline-block m-2">
-                    <Eventblock/>
-                    <Locationblock/>
-                </div> */}
             </>
 
         )
     }
 }
-
-
 
 export default SeperateView;
